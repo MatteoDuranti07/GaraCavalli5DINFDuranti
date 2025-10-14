2224e8dc3ecf1e5bd63e8f9429e7bc5311802324
Tommaso <rikypoggiani06@gmail.com>
2025-10-14 09:43:07 +0200

    primo commit, sleep per settare le lentezze

---
+++ src/Cavalli.java
@@ -0,0 +1,39 @@
+/*Questa classe è il nostro primo thread e consisterà la simulazione di un cavallo
+in corsa, parallelamente con altri dellla stessa specie
+@author Duranti
+@vesion 1.0
+*/
+
+public class Cavalli extends Thread{
+
+    private final String name;
+    private int lentezza;
+
+    public Cavalli(String name, int lentezza){
+        super();
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Cavallo:" + name + "comincia il suo galoppo");
+        for(int i = 1; i <= 10; i++){
+
+            try {
+                sleep(lentezza);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println(name + " cavalca - passo:" +i);
+        }
+    }
+
+    public int getLentezza() {
+        return lentezza;
+    }
+
+    public void setLentezza(int lentezza){
+        this.lentezza = lentezza;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner  = new Scanner(System.in);
+        int lentezza;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Inserisci lentezza di Fulmine:");
+        Cavalli fulmine  = new Cavalli("Fulmine",scanner.nextInt());
+        System.out.println("Inserisci lentezza di Fulmine:");
+        Cavalli lampo = new Cavalli("Lampo",scanner.nextInt());
+        System.out.println("Inserisci lentezza di Rose:");
+        Cavalli rose = new Cavalli("Rose",scanner.nextInt());
+        System.out.println("Inserisci lentezza di Margherita:");
+        Cavalli margherita = new Cavalli("Margherita",scanner.nextInt());
+        System.out.println("Inserisci lentezza di Rambo:");
+        Cavalli rambo = new Cavalli("Rambo",scanner.nextInt());
+
+//        rose.setPriority(Thread.MAX_PRIORITY);
+//        margherita.setPriority(Thread.MIN_PRIORITY);
+
+        fulmine.start();
+        lampo.start();
+        rose.start();
+        margherita.start();
+        rambo.start();
+
     }
 }
