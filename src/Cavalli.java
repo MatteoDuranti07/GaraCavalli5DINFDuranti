2592ce2b07f37184fda266eeca3c049e64e2ab3f
Tommaso <rikypoggiani06@gmail.com>
2025-10-21 09:27:30 +0200

    aggiornamento della classe cavalli e del main

--- src/Cavalli.java
+++ src/Cavalli.java
@@ -16,17 +16,19 @@ public class Cavalli extends Thread{
     }
 
     @Override
-    public void run() {
-        System.out.println("Cavallo:" + name + "comincia il suo galoppo");
-        for(int i = 1; i <= 10; i++){
-
+    public void run(){
+        System.out.println("Cavallo " +
+                name + " comincia il suo galoppo");
+        for (int i = 1; i <= 10 && !Thread.currentThread().isInterrupted() ; i++) {
             try {
                 sleep(lentezza);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-
-            System.out.println(name + " cavalca - passo:" +i);
+            System.out.println(name +" cavalca - passo: " + i);
+        }
+        if(Main.getPrimo().equals("")){
+            Main.setPrimo(this.name);
         }
     }
 
