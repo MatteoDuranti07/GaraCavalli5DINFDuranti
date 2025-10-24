fa42d8776a9c93f6aa23a855c514e2456da3dfa7
Tommaso <rikypoggiani06@gmail.com>
2025-10-24 13:16:23 +0200

    gara cavalli con azzopamento

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,96 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    static String primo="";
+    static String first;
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String tmpS;
-        int tmp;
-        ArrayList<Cavallo> listaCavallo = new ArrayList<Cavallo>();
-        for (int i = 1; i <= 4; i++) {
-            System.out.println("Inserisci il nome del cavallo " + i);
-            tmpS =  input.nextLine();
-            System.out.println("Inserisci la lentezza del cavallo " + i);
-            tmp = input.nextInt();
-            String v = input.nextLine(); //prende il \n
-            Cavallo c=new Cavallo(tmpS, tmp);
-            listaCavallo.add(c);
-        }
+        Scanner scanner = new Scanner(System.in);
+        boolean x = true;
+        ArrayList<Cavallo> horses = new ArrayList<>();
+        int condition;
+
+        while (x) {
+            System.out.println("__________________________________________________________________________");
+            System.out.println("1.Inserisci un cavallo\n2.Inserisci sleepTime\n3.Inizia la gara\n4.Azzoppa un cavallo\n5.Esci");
+            System.out.println("Inserisci un opzione:");
+            condition = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (condition) {
+                case 1:
+                    System.out.println("Inserisci il nome del cavallo:");
+                    String horseName = scanner.nextLine();
+                    horses.add(new Cavallo(horseName));
+                    break;
+
+                case 2:
+                    int i = 1;
+                    System.out.println("Lista cavalli:");
+                    for (Cavallo c : horses) {
+                        System.out.println(i++ + " " + c.getHorseName());
+                    }
+                    System.out.println("Inserisci nome cavallo:");
+                    String name = scanner.nextLine();
+                    System.out.println("Inserisci sleepTime:");
+                    int sleepTime = scanner.nextInt();
+                    scanner.nextLine();
+
+                    boolean found = false;
+                    for (Cavallo c : horses) {
+                        if (c.getHorseName().equals(name)) {
+                            c.setSleepTime(sleepTime);
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        System.out.println("Cavallo non trovato.");
+                    }
+                    break;
+
+                case 3:
+                    for (Cavallo c : horses) {
+                        c.start();
+                    }
+                    ////
+                    for (Cavallo c : horses) {
+                        try {
+                            c.join();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Il vincitore è: " + first);
+                    break;
+
+                case 4:
+                    if (!horses.isEmpty()) {
+                        int rand = (int) (Math.random() * horses.size());
+                        horses.get(rand).setInterrupt();
+                        System.out.println(horses.get(rand).getHorseName() + " è stato azzoppato!");
+                    }
+                    break;
 
 
-        for(Cavallo c: listaCavallo){
-            c.start();
-        }
-        for(Cavallo c: listaCavallo){
-            try {
-                c.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                case 5:
+                    x = false;
+                    break;
+
+                default:
+                    System.out.println("Funzione non valida!");
+                    break;
             }
         }
-        System.out.println("Il primo cavallo: " + primo);    }
 
-    public static String getPrimo() {
-        return primo;
+        scanner.close();
     }
-    public static void setPrimo(String primo) {
-        Main.primo = primo;
+
+    public static synchronized void setFirst(String t) {
+        first = t;
+    }
+
+    public static synchronized String getFirst() {
+        return first;
     }
 }
